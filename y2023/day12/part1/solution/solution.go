f327aeba4fc85cc26520aa26954c1b1319e1ccfa
Gabi Bizdoc <gabriel.bizdoc@gmail.com>
2023-12-18 06:55:34 +0200

    solve: day 12

--- y2023/day12/part1/solution/solution.go
+++ y2023/day12/part1/solution/solution.go
@@ -7,131 +7,29 @@ import (
 	"io"
 	"os"
 	"strings"
+	"sync"
+	"sync/atomic"
 )
 
-var globalMusk []int
-
-type PatterBuilder struct {
-	springs []Spring
-	musk    []int
-	m       int
-
-	cnt int
-}
-
-func NewPatterBuilder(musk []int) *PatterBuilder {
-	return &PatterBuilder{musk: musk}
-}
-
-func (b *PatterBuilder) Clone() *PatterBuilder {
-	return &PatterBuilder{
-		springs: com.CloneSlice(b.springs),
-		musk:    com.CloneSlice(b.musk),
-		m:       b.m,
-		cnt:     b.cnt,
-	}
-}
-
-func (b *PatterBuilder) IsSolution() bool {
-	return b.m == len(b.musk)
-}
-
-func (b *PatterBuilder) Prefix() string {
-	return string(b.springs)
-}
-
-func (b *PatterBuilder) Write(s Spring) bool {
-	b.springs = append(b.springs, s)
-	switch {
-	case s.IsDamaged():
-		b.cnt += 1
-		if b.m == len(b.musk) || b.cnt > b.musk[b.m] {
-			return false
-		}
-	case s.IsOperational():
-		if b.cnt == 0 {
-			return true // we don't care!
-		}
-		if b.m < len(b.musk) && b.cnt == b.musk[b.m] {
-			b.m += 1
-			b.cnt = 0
-		} else {
-			return false
-		}
-	}
-
-	return true
-}
-
-//var invalidSprings map[string]struct{}
-//
-//func init() {
-//	invalidSprings = make(map[string]struct{})
-//}
-
-func solveFunction(pattern []Spring, musk []int) int {
-	springs := make([]*PatterBuilder, 0)
-	springs = append(springs, NewPatterBuilder(musk))
-
-	for _, spring := range pattern {
-		if spring.IsUnknown() {
-			next := make([]*PatterBuilder, 0)
-			for _, builder := range springs {
-				b2 := builder.Clone()
-				if builder.Write('.') {
-					next = append(next, builder)
-				}
-				if b2.Write('#') {
-					next = append(next, b2)
-				}
-			}
-			springs = next
-		} else {
-			next := make([]*PatterBuilder, 0)
-			for _, builder := range springs {
-				if builder.Write(spring) {
-					next = append(next, builder)
-				}
-			}
-			springs = next
-		}
-
-		//fmt.Println("   ", string(pattern), musk)
-		//for i, builder := range springs {
-		//	fmt.Println(fmt.Sprintf("%3d", i), string(builder.springs), builder.cnt)
-		//}
-		//fmt.Println()
-	}
-
-	solution := 0
-	for _, spring := range springs {
-		if spring.IsSolution() {
-			solution += 1
-		}
-	}
-
-	//fmt.Println("   ", string(pattern), musk)
-	//for i, builder := range springs {
-	//	fmt.Println(fmt.Sprintf("%3d", i), string(builder.springs), builder.cnt, builder.IsSolution())
-	//}
-	//fmt.Println()
-	return solution
-
-	//return len(springs)
-}
-
-func findArrangements(pattern []Spring, musk []int) int {
-	globalMusk = musk
-	//fmt.Println(string(pattern), musk)
-	return solveFunction(pattern, musk)
-}
-
-func unpackLine(line string) ([]Spring, []int, error) {
+func unpackLine(line string) ([]rune, []int, error) {
 	fields := strings.Fields(line)
 	ints, err := com.ExtractIntSliceFromStringFunc(fields[1], func(r rune) bool {
 		return r == ','
 	})
-	springs := []Spring(fields[0])
+
+	// simplify
+	springs := make([]rune, 0, len(fields[0]))
+	var prev rune = ' '
+	for _, x := range fields[0] {
+		if x == '.' && prev == '.' {
+			continue
+		}
+
+		springs = append(springs, rune(x))
+		prev = x
+	}
+
+	// adding an extra dot to the end to simplify recursion
 	springs = append(springs, '.')
 	return springs, ints, err
 }
@@ -147,16 +45,58 @@ func Solve(file io.Reader) (solution int, err error) {
 		if err != nil {
 			return 0, err
 		}
-		res := findArrangements(pattern, match)
-		fmt.Println(pattern, match, res)
-		solution += res
-		//return 0, err
+
+		solution += solveLine(pattern, match)
 	}
 
 	if err := scanner.Err(); err != nil {
 		return solution, err
 	}
 
+	return solution, nil
+}
+
+func worker(wg *sync.WaitGroup, lineChan <-chan string, solution *atomic.Int64) {
+	defer wg.Done()
+
+	for line := range lineChan {
+		springs, pattern, err := unpackLine(line)
+		if err != nil {
+			panic(err)
+		}
+		sol := solveLine(springs, pattern)
+		solution.Add(int64(sol))
+	}
+}
+
+func Solve2(file io.Reader) (solution int, err error) {
+	scanner := bufio.NewScanner(file)
+
+	var maxWorkers = 30
+	var atomicSolution atomic.Int64
+	var wg sync.WaitGroup
+	lineChan := make(chan string)
+
+	for i := 0; i < maxWorkers; i++ {
+		wg.Add(1)
+		go worker(&wg, lineChan, &atomicSolution)
+	}
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+		lineChan <- line
+	}
+	close(lineChan)
+
+	if err := scanner.Err(); err != nil {
+		return solution, err
+	}
+	wg.Wait()
+
+	solution = int(atomicSolution.Load())
 	return solution, nil
 }
 
@@ -167,7 +107,7 @@ func solveChallenge(inputFilePath string) (int, error) {
 		return 0, err
 	}
 	defer file.Close()
-	return Solve(file)
+	return Solve2(file)
 }
 
 func SolveChallenge(inputFilePath string) (int, error) {
--- y2023/day12/part2/solution/solution.go
+++ y2023/day12/part2/solution/solution.go
@@ -3,161 +3,34 @@ package solution
 import (
 	"aoc/com"
 	"bufio"
-	"cmp"
 	"fmt"
 	"io"
 	"os"
-	"sort"
 	"strings"
+	"sync"
+	"sync/atomic"
 )
 
-var globalMusk []int
-
-type PatterBuilder struct {
-	springs []Spring
-	str     string
-	musk    []int
-	m       int
-
-	cnt int
-}
-
-func NewPatterBuilder(musk []int) *PatterBuilder {
-	return &PatterBuilder{musk: musk}
-}
-
-func (b *PatterBuilder) Clone() *PatterBuilder {
-	return &PatterBuilder{
-		springs: com.CloneSlice(b.springs),
-		musk:    com.CloneSlice(b.musk),
-		m:       b.m,
-		cnt:     b.cnt,
-	}
-}
-
-func (b *PatterBuilder) IsSolution() bool {
-	return b.m == len(b.musk)
-}
-
-func (b *PatterBuilder) Prefix() string {
-	return string(b.springs)
-}
-
-func (b *PatterBuilder) Write(s Spring) bool {
-	defer func() {
-		b.str = string(b.springs)
-	}()
-
-	b.springs = append(b.springs, s)
-	switch {
-	case s.IsDamaged():
-		b.cnt += 1
-		if b.m == len(b.musk) || b.cnt > b.musk[b.m] {
-			return false
-		}
-	case s.IsOperational():
-		if b.cnt == 0 {
-			return true // we don't care!
-		}
-		if b.m < len(b.musk) && b.cnt == b.musk[b.m] {
-			b.m += 1
-			b.cnt = 0
-		} else {
-			return false
-		}
-	}
-
-	return true
-}
-
-var invalidSprings map[string]int
-
-func init() {
-	invalidSprings = make(map[string]int)
-}
-func solveFunction22(pattern []Spring, musk []int) int {
-	springs := make([]*PatterBuilder, 0)
-	springs = append(springs, NewPatterBuilder(musk))
-
-	for _, spring := range pattern {
-		if spring.IsUnknown() {
-			next := make([]*PatterBuilder, 0)
-			for _, builder := range springs {
-				b2 := builder.Clone()
-				if builder.Write('.') {
-					next = append(next, builder)
-					//if invalidSprings[builder.Prefix()] == 0 {
-					//	next = append(next, builder)
-					//	invalidSprings[builder.Prefix()] += 1
-					//}
-				}
-				if b2.Write('#') {
-					next = append(next, b2)
-					//if invalidSprings[b2.Prefix()] == 0 {
-					//	next = append(next, b2)
-					//	invalidSprings[b2.Prefix()] += 1
-					//}
-				}
-			}
-			springs = next
-		} else {
-			next := make([]*PatterBuilder, 0)
-			for _, builder := range springs {
-				if builder.Write(spring) {
-					next = append(next, builder)
-					//if invalidSprings[builder.Prefix()] == 0 {
-					//	next = append(next, builder)
-					//	invalidSprings[builder.Prefix()] += 1
-					//}
-				}
-			}
-			springs = next
-		}
-
-		sort.Slice(springs, func(i, j int) bool {
-			return cmp.Compare(springs[i].str, springs[j].str) > 0
-		})
-
-		fmt.Println(" ")
-		for i, builder := range springs {
-			//fmt.Println(i, string(builder.springs))
-			fmt.Println(i, builder.str)
-		}
-		fmt.Println(" ")
-
-	}
-
-	solution := 0
-	for _, spring := range springs {
-		if spring.IsSolution() {
-			solution += 1
-			//fmt.Println(string(spring.springs))
-		}
-	}
-
-	return solution
-}
-
-func unpackLine(line string) ([]Spring, []int, error) {
+func unpackLine(line string) ([]rune, []int, error) {
 	fields := strings.Fields(line)
 	ints, err := com.ExtractIntSliceFromStringFunc(fields[1], func(r rune) bool {
 		return r == ','
 	})
 
 	// simplify
-	//springs := make([]Spring, 0)
-	//var prev rune = ' '
-	//for _, x := range fields[0] {
-	//	prev = x
-	//	if x != '.' && prev != '.' {
-	//		springs = append(springs, Spring(x))
-	//	}
-	//}
-	//fmt.Println("==-", springs)
-	springs := []Spring(fields[0])
+	springs := make([]rune, 0, len(fields[0]))
+	var prev rune = ' '
+	for _, x := range fields[0] {
+		if x == '.' && prev == '.' {
+			continue
+		}
+
+		springs = append(springs, rune(x))
+		prev = x
+	}
 
 	// clone
-	springs2 := make([]Spring, 0, len(springs)*5+5)
+	springs2 := make([]rune, 0, len(springs)*5+5)
 	ints2 := make([]int, 0, len(ints)*5)
 
 	for i := 0; i < 5; i++ {
@@ -165,9 +38,9 @@ func unpackLine(line string) ([]Spring, []int, error) {
 		springs2 = append(springs2, '?')
 		ints2 = append(ints2, ints...)
 	}
+
+	// adding an extra dot to the end to simplify recursion
 	springs2[len(springs2)-1] = '.'
-	//springs2 = append(springs2, springs...)
-	//springs2 = append(springs, '.')
 	return springs2, ints2, err
 }
 
@@ -183,13 +56,57 @@ func Solve(file io.Reader) (solution int, err error) {
 			return 0, err
 		}
 
-		solution += solve(pattern, match)
+		solution += solveLine(pattern, match)
 	}
 
 	if err := scanner.Err(); err != nil {
 		return solution, err
 	}
 
+	return solution, nil
+}
+
+func worker(wg *sync.WaitGroup, lineChan <-chan string, solution *atomic.Int64) {
+	defer wg.Done()
+
+	for line := range lineChan {
+		springs, pattern, err := unpackLine(line)
+		if err != nil {
+			panic(err)
+		}
+		sol := solveLine(springs, pattern)
+		solution.Add(int64(sol))
+	}
+}
+
+func Solve2(file io.Reader) (solution int, err error) {
+	scanner := bufio.NewScanner(file)
+
+	var maxWorkers = 30
+	var atomicSolution atomic.Int64
+	var wg sync.WaitGroup
+	lineChan := make(chan string, maxWorkers)
+
+	for i := 0; i < maxWorkers; i++ {
+		wg.Add(1)
+		go worker(&wg, lineChan, &atomicSolution)
+	}
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
+			continue
+		}
+		lineChan <- line
+	}
+	close(lineChan)
+
+	if err := scanner.Err(); err != nil {
+		return solution, err
+	}
+	wg.Wait()
+
+	solution = int(atomicSolution.Load())
 	return solution, nil
 }
 
@@ -200,7 +117,7 @@ func solveChallenge(inputFilePath string) (int, error) {
 		return 0, err
 	}
 	defer file.Close()
-	return Solve(file)
+	return Solve2(file)
 }
 
 func SolveChallenge(inputFilePath string) (int, error) {
